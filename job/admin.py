f5af11823052f11fed21788b08cc117465ce5e7e
remon <remon9015@gmail.com>
2020-10-19 12:32:19 +0200

    add job views$url

--- job/admin.py
+++ job/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import job
+from .models import job, category
+
 
 admin.site.register(job)
+
+admin.site.register(category)
